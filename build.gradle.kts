7e8a82c5e3004c7beb062853d56d4b933a883d4f
jthies73 <johannes.thies.73@gmail.com>
2026-01-17 03:02:32 +0000

    basic gradle setup

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "2.3.0" // Kotlin version to use
+    application
+}
+
+group = "org.example" // A company name, for example, `org.jetbrains`
+version = "1.0-SNAPSHOT" // Version to assign to the built artifact
+
+application {
+    mainClass.set("MainKt")
+}
+
+repositories { // Sources of dependencies. See 1️⃣
+    mavenCentral() // Maven Central Repository. See 2️⃣
+}
+
+dependencies { // All the libraries you want to use. See 3️⃣
+    // Copy dependencies' names after you find them in a repository
+    testImplementation(kotlin("test")) // The Kotlin test library
+}
+
+tasks.test { // See 4️⃣
+    useJUnitPlatform() // JUnitPlatform for tests. See 5️⃣
+}
